469a2939b6a4af012ac5c591e68ec238a15cacd9
LXL1103 <114650659+LXL1103@users.noreply.github.com>
2023-04-21 00:08:39 -0700

    first commit testing

---
+++ Final Project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Final Project
+//
+//  Created by Man Doan on 4/21/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
