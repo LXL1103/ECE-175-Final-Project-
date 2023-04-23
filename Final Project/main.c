22f2c378eb014f17d96a37f1c3573e21ce0e1124
Em <itzadbzfan@gmail.com>
2023-04-22 21:13:24 -0700

    1

--- Final Project/main.c
+++ Final Project/main.c
@@ -5,12 +5,13 @@
 //  Created by Man Doan on 4/21/23.
 //
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
     printf("pp");
-    printf("test");
+    printf("Hi");
     return 0;
 }
